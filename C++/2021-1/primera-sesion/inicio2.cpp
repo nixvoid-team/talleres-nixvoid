9e7e61384a05805bd7e9de4f57282f1a8e0a5406
Rony <rony.hancco@gmail.com>
2021-08-09 18:47:25 -0500

    primera sesion C++

---
+++ C++/2021-1/primera-sesion/inicio2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int edad = 0;
+  cout << "Su edad es: "<< edad << endl;
+  edad = edad + 10;
+  cout << "Su edad es: "<< edad << endl;
+  edad = edad * 10;
+  cout << "Su edad es: "<< edad << endl;
+  edad = edad - 5;
+  cout << "Su edad es: "<< edad << endl;
+  edad = edad / 2;
+  cout << "Su edad es: "<< edad << endl;
+  return edad;
+}
