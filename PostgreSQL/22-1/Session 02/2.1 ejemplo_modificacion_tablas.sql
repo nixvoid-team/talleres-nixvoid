ea77972beea2a711aec46ee59df2775370b7231b
David Yancce <david.yanccehuallpa.q@uni.pe>
2022-08-25 19:58:50 -0500

    feat: PostgreSQL session 02 después del taller

--- PostgreSQL/22-1/Session 02/2.1 ejemplo_modificacion_tablas.sql
+++ PostgreSQL/22-1/Session 02/2.1 ejemplo_modificacion_tablas.sql
@@ -2,3 +2,16 @@
 -- Cambiar el nombre de PASSWORD a secret_password
 -- Eliminar la columna activo
 -- Cambiar el nombre de la tabla 'cuentas' a 'usuarios'
+ALTER TABLE
+    cuentas
+ADD
+    COLUMN activo BOOLEAN NOT NULL;
+
+ALTER TABLE
+    cuentas RENAME COLUMN "password" TO secret_password;
+
+ALTER TABLE
+    cuentas DROP COLUMN activo;
+
+ALTER TABLE
+    cuentas RENAME TO usuarios
